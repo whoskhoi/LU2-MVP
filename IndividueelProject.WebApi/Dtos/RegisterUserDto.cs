0aef50ad357f78c3becdcb783f783418a701e249
whoskhoi <khoivaniersel@gmail.com>
2025-04-13 10:08:54 +0200

    Readded rejects

--- IndividueelProject.WebApi/Dtos/RegisterUserDto.cs
+++ IndividueelProject.WebApi/Dtos/RegisterUserDto.cs
@@ -10,10 +10,10 @@ namespace IndividueelProject.WebApi.Dtos
 
 
         [Required]
-        /* [RegularExpression(
+         [RegularExpression(
             "^(?=.[A-Z])(?=.[a-z])(?=.\\d)(?=.[!@#$%^&()_+\\-=])[A-Za-z\\d!@#$%^&()_+\\-=]{10,}$",
             ErrorMessage = "Password must be at least 10 characters, contain uppercase, lowercase, digit, and special character."
-        )] */
+        )] 
         public string Password { get; set; } = null!;
     }
 }
